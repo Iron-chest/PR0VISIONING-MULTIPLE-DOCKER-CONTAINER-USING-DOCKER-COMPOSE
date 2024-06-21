56719ba434bf40a7200529b1d177f8359284a7d3
AKINTAYO TOLANI <devopsforjesus@gmail.com>
2024-06-21 12:46:56 +0100

    Update and rename docker.sh to docker-compose.sh

---
+++ Scripts/docker-compose.sh
@@ -0,0 +1,3 @@
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
